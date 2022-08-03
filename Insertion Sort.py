61978b1339770eb9a927f1d32fad57ec0818ca0b
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-03 18:10:14 +0530

    Create Insertion Sort.py

---
+++ Insertion Sort.py
@@ -0,0 +1,15 @@
+# INSERTION SORT
+
+a = [10290,15293,2412,324,23813,3234,29384,2585,219,83295,648746,233]
+
+for i in range(1,len(a)):
+    
+    temp = a[i]
+    j = i-1
+    
+    while j>=0 and a[j]>temp:
+        a[j+1] = a[j]
+        j-=1
+    a[j+1] = temp
+
+print(a)
