fc7571fdf6ed56b82913c798c84a1fef38f56b25
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-25 13:34:52 +0530

    Create Count Sort.py

---
+++ Count Sort.py
@@ -0,0 +1,24 @@
+
+arr = [3,2,4,3,5,6,3,3,5,4,3,2,1,3,5,4,3,2,2,5,5,6,6]
+
+mx = max(arr)
+frequency = [0]*(mx+1)
+n = len(arr)
+
+for i in arr:
+    frequency[i] += 1
+
+result = [0]*n
+
+for i in range(1,mx+1):
+    frequency[i] += frequency[i-1]
+    
+
+for i in range(n-1,-1,-1):
+    frequency[arr[i]] -= 1
+    result[frequency[arr[i]]] = arr[i]
+    
+for i in range(n):
+    arr[i] = result[i]
+    
+print(arr)
